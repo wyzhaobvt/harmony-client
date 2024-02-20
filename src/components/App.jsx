b9b99338858e4bea1b64fb7f01b3b7bc3ca71d02
Gerrime Dioso <gdioso98@gmail.com>
2024-02-20 15:22:13 -0800

    setup layout for register page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
+import Register from "../pages/Register"
+
 function App() {
   return (
     <>
-      <h1 className="text-center text-3xl font-bold">Harmony</h1>
+      {/* <h1 className="text-center text-3xl font-bold">Harmony</h1> */}
+      <Register />
     </>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Register = () => {
+  return (
+    <Card className="w-[350px] md:w-[423px]">
+      <CardHeader>
+        <CardTitle>Create an Account</CardTitle>
+        <CardDescription>
+          Welcome to Harmony, where teamwork thrives and collaboration blossoms.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            {/* First name */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="firstName">First Name</Label>
+              <Input id="firstName" placeholder="First Name" />
+            </div>
+            {/* Last name */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="lastName">Last Name</Label>
+              <Input id="lastName" placeholder="Last Name" />
+            </div>
+            {/* Email */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" validate />
+            </div>
+            {/* Password */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" placeholder="Password" />
+            </div>
+            {/* Confirm Password */}
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="confirmPassword">Confirm Password</Label>
+              <Input id="confirmPassword" placeholder="Confirm Password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-end pb-3">
+        <Button variant="outline">Create</Button>
+      </CardFooter>
+      <CardFooter className="flex justify-start">
+        <CardDescription>
+          Already have an account? <span className="underline cursor-pointer">Log In</span>
+        </CardDescription>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default Register;
