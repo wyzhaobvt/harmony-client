5e95319711dd6a2a0b05847a04d577206425a022
Egor <65542192+Sillor@users.noreply.github.com>
2024-02-23 13:17:47 -0800

    Refactor CSS and add GroupDashboard component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './css/App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx';
+import GroupDashboard from './pages/GroupDashboard.jsx';
+import './css/App.css';
+import './css/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <GroupDashboard />
+  </React.StrictMode>
+);
