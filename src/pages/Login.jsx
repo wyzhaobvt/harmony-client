50bf593b13a5883fb77fe09ba9c0716aad058795
Gerrime Dioso <gdioso98@gmail.com>
2024-02-23 16:14:44 -0800

    Centered card and changed border color of the card when in dark mode

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,38 +12,42 @@ import { Label } from "@/components/ui/label";
 
 const Login = () => {
   return (
-    <Card className="w-[350px] md:w-[428px]">
-      <CardHeader>
-        <CardTitle className="mb-3">Log in</CardTitle>
-        <CardDescription>
-          Welcome to Harmony, where teamwork thrives and collaboration blossoms.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            {/* Email */}
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Email</Label>
-              <Input type="email" id="email" placeholder="Email" />
+    <div className="h-[calc(100vh-6rem)] flex items-center">
+      <Card className="w-[350px] md:w-[428px] dark:border-neutral-800">
+        <CardHeader>
+          <CardTitle className="mb-3">Log in</CardTitle>
+          <CardDescription>
+            Welcome to Harmony, where teamwork thrives and collaboration
+            blossoms.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              {/* Email */}
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+              </div>
+              {/* Password */}
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" placeholder="Password" />
+              </div>
             </div>
-            {/* Password */}
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input type="password" id="password" placeholder="Password" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-end pb-3">
-        <Button variant="outline">Log In</Button>
-      </CardFooter>
-      <CardFooter className="flex justify-start">
-        <CardDescription>
-          Don't have an account? <span className="underline cursor-pointer">Sign Up</span>
-        </CardDescription>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-end pb-3">
+          <Button variant="outline">Log In</Button>
+        </CardFooter>
+        <CardFooter className="flex justify-start">
+          <CardDescription>
+            Don't have an account?{" "}
+            <span className="underline cursor-pointer">Sign Up</span>
+          </CardDescription>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
