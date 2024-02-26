76f5540013f1bcde50dfe6cce766542472605628
Gerrime Dioso <gdioso98@gmail.com>
2024-02-26 15:54:07 -0800

    Added className prop

--- src/components/ui/password-visibility-toggle.jsx
+++ src/components/ui/password-visibility-toggle.jsx
@@ -1,16 +1,16 @@
 import { EyeOpenIcon, EyeClosedIcon } from "@radix-ui/react-icons";
 
-const PasswordVisibilityToggle = ({ isHidden, handleToggle }) => {
+const PasswordVisibilityToggle = ({ isHidden, handleToggle, className }) => {
   return (
     <>
       {isHidden ? (
         <EyeOpenIcon
-          className="ml-2 h-4 w-4 absolute inset-y-3 right-3 cursor-pointer select-none"
+          className={`ml-2 h-4 w-4 absolute inset-y-3 right-3 cursor-pointer select-none ${className}`}
           onClick={handleToggle}
         />
       ) : (
         <EyeClosedIcon
-          className="ml-2 h-4 w-4 absolute inset-y-3 right-3 cursor-pointer select-none"
+          className={`ml-2 h-4 w-4 absolute inset-y-3 right-3 cursor-pointer select-none ${className}`}
           onClick={handleToggle}
         />
       )}
