2577c901223273f05089da11b006978993fecb4b
Gerrime Dioso <gdioso98@gmail.com>
2024-03-01 14:02:04 -0800

    Implemented input validation

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,18 +1,61 @@
+import { useState, useEffect } from "react";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { AvatarIcon } from "@radix-ui/react-icons";
 import { EditProfilePicture } from "./EditProfilePicture";
+import StatusMessage from "../../components/ui/status-message";
+
+// Dummy data. Will be replaced by data of current user from database.
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "jdoe@email.com"
+}
 
 const Profile = () => {
-  const handleOnChange = () => {
-    console.log("Handle change");
+  const [userData, setUserData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email
+  });
+
+  const [errors, setErrors] = useState({
+    firstName: null,
+    lastName: null,
+    email: null
+  });
+
+  const handleOnChange = (event) => {
+    setUserData(prev => ({ ...prev, [event.target.name]: event.target.value }))
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Profile updated");
+
+    // For each 'userData' field, set the corresponding error to false if the inputData value is a truthy.
+    // Else, set the error to be true.
+    Object.keys(userData).forEach((field) => {
+      const value = userData[field];
+      setErrors((prev) => ({ ...prev, [field]: value ? false : true }));
+    });
   };
+
+  const finishSubmit = () => {
+    console.log("Profile updated");
+  }
+
+  useEffect(() => {
+    // Set 'isErrors' to true if 'errors' has at least one truthy value or one null value
+    const isErrors = Object.values(errors).some(
+      (error) => error === true || error === null
+    );
+
+    if (!isErrors) {
+      finishSubmit();
+    }
+  }, [errors]);
 
   return (
     <div className="h-[calc(100vh-6rem)] w-[250px] sm:w-[420px] flex flex-col justify-center">
@@ -36,7 +79,13 @@ const Profile = () => {
               id="firstName"
               name="firstName"
               placeholder="First Name"
+              defaultValue={userData.firstName}
               onChange={handleOnChange}
+            />
+            {/* Error for First Name field */}
+            <StatusMessage
+              error={errors.firstName}
+              message="First Name field is required"
             />
           </div>
           {/* Last Name */}
@@ -46,17 +95,30 @@ const Profile = () => {
               id="lastName"
               name="lastName"
               placeholder="Last Name"
+              defaultValue={userData.lastName}
               onChange={handleOnChange}
+            />
+            {/* Error for Last Name field */}
+            <StatusMessage
+              error={errors.lastName}
+              message="Last Name field is required"
             />
           </div>
           {/* Email */}
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="email">Email</Label>
             <Input
+              type="email"
               id="email"
               name="email"
               placeholder="Email"
+              defaultValue={userData.email}
               onChange={handleOnChange}
+            />
+            {/* Error for Email field */}
+            <StatusMessage
+              error={errors.email}
+              message="Email field is required"
             />
           </div>
           {/* Update Profile Button */}
