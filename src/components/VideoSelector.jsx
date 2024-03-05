360a9684feed20a508e01795c9da296a4db57320
Nolan Lawler <nlawler0207@gmail.com>
2024-03-05 15:45:25 -0800

    bug fixes, mobile support

--- src/components/VideoSelector.jsx
+++ src/components/VideoSelector.jsx
@@ -15,12 +15,17 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useEffect, useState } from "react";
+import isMobileDevice from "../utils/isMobileDevice";
 
 export default function VideoSelector({ triggerButton, onStream }) {
   const [mediaDevices, setMediaDevices] = useState([]);
   const [selectedCamera, setSelectedCamera] = useState(null);
-  const [streams, setStreams] = useState({myStream: true, camera: null, display: null})
-  const hasPermission = "mediaDevices" in navigator
+  const [streams, setStreams] = useState({
+    myStream: true,
+    camera: null,
+    display: null,
+  });
+  const hasPermission = "mediaDevices" in navigator;
 
   useEffect(() => {
     navigator.mediaDevices?.enumerateDevices().then((devices) => {
@@ -32,25 +37,35 @@ export default function VideoSelector({ triggerButton, onStream }) {
 
   function handleSelectDisplayClick() {
     if (streams.display) {
-      streams.display.getTracks().forEach(track=>{
-        track.stop()
-      })
+      streams.display.getTracks().forEach((track) => {
+        track.stop();
+      });
       const obj = {
         ...streams,
-        display: null
-      }
-      setStreams(obj)
+        display: null,
+      };
+      setStreams(obj);
       if (typeof onStream === "function") onStream(obj);
-      return
+      return;
     }
     navigator.mediaDevices
       .getDisplayMedia({ video: true, audio: true })
       .then((stream) => {
+        stream.getTracks().forEach((track) => {
+          track.addEventListener("ended", () => {
+            const obj = {
+              ...streams,
+              display: null,
+            };
+            setStreams(obj);
+            if (typeof onStream === "function") onStream(obj);
+          });
+        });
         const obj = {
           ...streams,
-          display: stream
-        }
-        setStreams(obj)
+          display: stream,
+        };
+        setStreams(obj);
         if (typeof onStream === "function") onStream(obj);
       })
       .catch(() => {});
@@ -58,30 +73,40 @@ export default function VideoSelector({ triggerButton, onStream }) {
 
   function handleSelectCameraClick() {
     if (streams.camera) {
-      streams.camera.getTracks().forEach(track=>{
-        track.stop()
-      })
+      streams.camera.getTracks().forEach((track) => {
+        track.stop();
+      });
       const obj = {
         ...streams,
-        camera: null
-      }
-      setStreams(obj)
+        camera: null,
+      };
+      setStreams(obj);
       if (typeof onStream === "function") onStream(obj);
-      return
+      return;
     }
     navigator.mediaDevices
       .getUserMedia({
         video:
           selectedCamera === "default"
             ? true
-            : { deviceId: {exact: mediaDevices[selectedCamera].deviceId} },
+            : selectedCamera === "front" ? {facingMode: {ideal: "user"}} : selectedCamera === "rear" ? {facingMode: {ideal: "environment"}} : { deviceId: { exact: mediaDevices[selectedCamera].deviceId } },
       })
       .then((stream) => {
+        stream.getTracks().forEach((track) => {
+          track.addEventListener("ended", () => {
+            const obj = {
+              ...streams,
+              camera: null,
+            };
+            setStreams(obj);
+            if (typeof onStream === "function") onStream(obj);
+          });
+        });
         const obj = {
           ...streams,
           camera: stream,
-        }
-        setStreams(obj)
+        };
+        setStreams(obj);
         if (typeof onStream === "function") onStream(obj);
       })
       .catch(() => {});
@@ -92,14 +117,30 @@ export default function VideoSelector({ triggerButton, onStream }) {
       <PopoverTrigger asChild>{triggerButton}</PopoverTrigger>
       <PopoverContent className="w-auto h-auto border-input m-5">
         <div className="flex gap-5">
-          <div className="flex items-center">
-            <Button disabled={!hasPermission} className={`${streams.display && "text-red-600"}`} onClick={handleSelectDisplayClick}>{ streams.display ? "Stop Display" : "Select Display"}</Button>
-          </div>
-          <div className="h-auto flex flex-col justify-center items-center">
-            <Separator orientation="vertical" className="h-2/5 w-[2px]" />
-            <span className={`text-zinc-600 ${!hasPermission && "!text-red-300"}`}>{hasPermission ? "or" : "No Permission"}</span>
-            <Separator orientation="vertical" className="h-2/5 w-[2px]" />
-          </div>
+          {!isMobileDevice() && (
+            <>
+              <div className="flex items-center">
+                <Button
+                  disabled={!hasPermission}
+                  className={`${streams.display && "text-red-600"}`}
+                  onClick={handleSelectDisplayClick}
+                >
+                  {streams.display ? "Stop Display" : "Select Display"}
+                </Button>
+              </div>
+              <div className="h-auto flex flex-col justify-center items-center">
+                <Separator orientation="vertical" className="h-2/5 w-[2px]" />
+                <span
+                  className={`text-zinc-600 ${
+                    !hasPermission && "!text-red-300"
+                  }`}
+                >
+                  {hasPermission ? "or" : ""}
+                </span>
+                <Separator orientation="vertical" className="h-2/5 w-[2px]" />
+              </div>
+            </>
+          )}
           <div className="flex flex-col items-center">
             <Select
               onValueChange={(value) => {
@@ -112,14 +153,23 @@ export default function VideoSelector({ triggerButton, onStream }) {
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Cameras</SelectLabel>
-                  <SelectItem value="default">Default</SelectItem>
-                  {mediaDevices.map((device, i) => {
-                    return (
-                      <SelectItem key={i} value={i}>
-                        {device.label || "Camera " + (i + 1)}
-                      </SelectItem>
-                    );
-                  })}
+                  {!isMobileDevice() ? (
+                    <>
+                      <SelectItem value="default">Default</SelectItem>
+                      {mediaDevices.map((device, i) => {
+                        return (
+                          <SelectItem key={i} value={i}>
+                            {device.label || "Camera " + (i + 1)}
+                          </SelectItem>
+                        );
+                      })}
+                    </>
+                  ) : (
+                    <>
+                      <SelectItem value="front">Front</SelectItem>
+                      <SelectItem value="rear">Rear</SelectItem>
+                    </>
+                  )}
                 </SelectGroup>
               </SelectContent>
             </Select>
--- src/components/VideoStream.jsx
+++ src/components/VideoStream.jsx
@@ -1,11 +1,31 @@
 import { useEffect, useRef, useState } from "react";
+import {Button} from "@/components/ui/button"
+import { FullscreenIcon } from "lucide-react";
+import { EnterFullScreenIcon } from "@radix-ui/react-icons";
+import isMobileDevice from "../utils/isMobileDevice";
 
-export default function VideoStream({ displayStream, cameraStream, className, ...props }) {
+export default function VideoStream({
+  displayStream,
+  cameraStream,
+  className,
+  ...props
+}) {
   const mainVideo = useRef(null);
   const secondaryVideo = useRef(null);
   const [swapStream, setSwapStream] = useState(false);
-  const mainStream = displayStream && cameraStream ? (swapStream ? cameraStream : displayStream) : displayStream || cameraStream || null
-  const secondaryStream = !displayStream ? null : cameraStream ? (swapStream ? displayStream : cameraStream) : cameraStream || null
+  const mainStream =
+    displayStream && cameraStream
+      ? swapStream
+        ? cameraStream
+        : displayStream
+      : displayStream || cameraStream || null;
+  const secondaryStream = !displayStream
+    ? null
+    : cameraStream
+    ? swapStream
+      ? displayStream
+      : cameraStream
+    : cameraStream || null;
   useEffect(() => {
     if (mainStream) mainVideo.current.srcObject = mainStream;
     if (secondaryStream) secondaryVideo.current.srcObject = secondaryStream;
@@ -13,10 +33,42 @@ export default function VideoStream({ displayStream, cameraStream, className, ..
   function onSecondaryClick(event) {
     event.preventDefault();
     event.stopPropagation();
-    setSwapStream(prev=>!prev)
+    setSwapStream((prev) => !prev);
   }
-  return <div className={`${className} relative`}>
-    {mainStream && <video ref={mainVideo} className={className + " relative !w-full"} {...props} autoPlay playsInline></video>}
-    {secondaryStream && <video ref={secondaryVideo} className={className + " " + "absolute top-2 right-2 !w-[25%] outline outline-1 !rounded-sm"} {...props} autoPlay playsInline onClick={onSecondaryClick}></video>}
-  </div>
+  return (
+    <div className={`${className} relative`}>
+      
+      {!isMobileDevice() && <div className="bg-[linear-gradient(0deg,black,#00000066,transparent)] w-full h-14 absolute bottom-0 z-10 group-hover:flex hidden items-center px-4 justify-end">
+        <EnterFullScreenIcon className="h-6 w-6 cursor-pointer" onClick={()=>{
+          const el =  mainVideo.current
+          if (el.requestFullScreen) {
+            el.requestFullScreen();
+          } else if (el.mozRequestFullScreen) {
+            el.mozRequestFullScreen();
+          } else if (el.webkitRequestFullScreen) {
+            el.webkitRequestFullScreen();
+          }
+        }}/>
+      </div>}
+      {mainStream && (
+        <video
+          ref={mainVideo}
+          className={`${className} relative !w-full`}
+          {...props}
+          autoPlay
+          playsInline
+        ></video>
+      )}
+      {secondaryStream && (
+        <video
+          ref={secondaryVideo}
+          className={`${className} absolute top-2 right-2 !w-[25%] outline outline-1 !rounded-sm`}
+          {...props}
+          autoPlay
+          playsInline
+          onClick={onSecondaryClick}
+        ></video>
+      )}
+    </div>
+  );
 }
