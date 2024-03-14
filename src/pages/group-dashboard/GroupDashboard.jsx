f92d921cd58b7edd88027890114c7ecb9f7799b7
joshluca98 <joshluca98@gmail.com>
2024-03-13 17:43:32 -0700

    removed redundant line

--- src/pages/group-dashboard/GroupDashboard.jsx
+++ src/pages/group-dashboard/GroupDashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import Message from './Message';
 import Utils from './Utils';
 import { Phone, Calendar, UserRoundPlus } from 'lucide-react';
