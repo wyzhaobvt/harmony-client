589ce04aea1198991fe6e4a7a4cf06a5284c86d0
Nolan Lawler <nlawler0207@gmail.com>
2024-04-19 14:00:25 -0700

    added file change listeners

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import { Outlet, useNavigate, useLocation } from "react-router";
 import CallRequestAlert from "./CallRequestAlert";
 import Navbar from "./Navbar";
 import setTheme from "../utils/setTheme";
-import { peer, socket, AppContext } from "../utils/globals";
-import { useToast } from "@/components/ui/use-toast"
+import globals, { peer, socket, AppContext } from "../utils/globals";
+import { useToast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   const [callAlert, setCallAlert] = useState(null);
   const [teamNotifications, setTeamNotifications] = useState({});
-  const [teamUpdated, setTeamUpdated] = useState({})
+  const [teamUpdated, setTeamUpdated] = useState({});
   const navigate = useNavigate();
   const location = useLocation();
   const audioRef = useRef(null);
@@ -21,16 +21,17 @@ function App() {
   useEffect(() => {
     const [, group, , uid] = location.pathname.split("/");
     if (group === "group" && teamNotifications[uid]) {
-      setTeamNotifications(prev=>{
-        const obj = {...prev}
-        const val = obj[uid]
-        obj[uid] = val === null ? -1 : val > 0 ? null : val - 1
-        return obj
-      })
+      setTeamNotifications((prev) => {
+        const obj = { ...prev };
+        const val = obj[uid];
+        obj[uid] = val === null ? -1 : val > 0 ? null : val - 1;
+        return obj;
+      });
     }
   }, [location]);
 
   useEffect(() => {
+    const path = location.pathname.split("/").slice(1)
     function onNewMessage({ team }) {
       setTeamNotifications((prev) => {
         const obj = { ...prev };
@@ -38,24 +39,24 @@ function App() {
         obj[team]++;
         return obj;
       });
-      setTeamUpdated(prev=>{
-        const obj = {[team]: (prev[team] || 0) + 1}
-        return obj
-      })
-    }
-    
-    function onEditedMessage({team}) {
-      setTeamUpdated(prev=>{
-        const obj = {[team]: (prev[team] || 0) + 1}
-        return obj
-      })
+      setTeamUpdated((prev) => {
+        const obj = { [team]: (prev[team] || 0) + 1 };
+        return obj;
+      });
     }
 
-    function onDeletedMessage({team}) {
-      setTeamUpdated(prev=>{
-        const obj = {[team]: (prev[team] || 0) + 1}
-        return obj
-      })
+    function onEditedMessage({ team }) {
+      setTeamUpdated((prev) => {
+        const obj = { [team]: (prev[team] || 0) + 1 };
+        return obj;
+      });
+    }
+
+    function onDeletedMessage({ team }) {
+      setTeamUpdated((prev) => {
+        const obj = { [team]: (prev[team] || 0) + 1 };
+        return obj;
+      });
     }
 
     function onCallInvite({ socketId, username }, accept, decline) {
@@ -91,22 +92,50 @@ function App() {
       }
     }
 
+    function onFileAdded({ team, filename, user }) { 
+      if (path[0] === "files" && path[1] === team) return
+      toast({
+        title: "File Added",
+        description: `${user} added '${filename}' to ${
+          globals.teamsCache[team]?.name || "?"
+        }`,
+        className: "px-3 py-1",
+      });
+    }
+
+    function onFileRemoved({ team, filename, user }) {
+      if (path[0] === "files" && path[1] === team) return
+      toast({
+        title: "File Removed",
+        description: `${user} removed '${filename}' from ${
+          globals.teamsCache[team]?.name || "?"
+        }`,
+        className: "px-3 py-1",
+      });
+    }
+
     peer.addEventListener("callInvite", onCallInvite);
     peer.addEventListener("receivingChanged", onReceivingChanged);
     socket.on("update:new_message", onNewMessage);
-    socket.on("update:edited_message", onEditedMessage)
-    socket.on("update:deleted_message", onDeletedMessage)
-    
+    socket.on("update:edited_message", onEditedMessage);
+    socket.on("update:deleted_message", onDeletedMessage);
+    socket.on("update:file_added", onFileAdded);
+    socket.on("update:file_removed", onFileRemoved);
+
     return () => {
       peer.removeEventListener("callInvite", onCallInvite);
       peer.removeEventListener("receivingChanged", onReceivingChanged);
       socket.off("update:new_message", onNewMessage);
-      socket.off("update:edited_message", onEditedMessage)
-      socket.off("update:deleted_message", onDeletedMessage)
+      socket.off("update:edited_message", onEditedMessage);
+      socket.off("update:deleted_message", onDeletedMessage);
+      socket.off("update:file_added", onFileAdded);
+      socket.off("update:file_removed", onFileRemoved);
     };
-  }, []);
+  }, [location]);
   return (
-    <AppContext.Provider value={{ teamNotifications, setTeamNotifications, teamUpdated }}>
+    <AppContext.Provider
+      value={{ teamNotifications, setTeamNotifications, teamUpdated }}
+    >
       <Navbar />
       <main>
         <Outlet />
